ed9d37cb84cbefc27b6e9fa62d024c150c7b469e
Abhishek Kumar <45291137+abhi7743@users.noreply.github.com>
2023-04-17 01:34:41 +0530

    work experience

--- force-app/main/default/lwc/workExperienceTab/workExperienceTab.js
+++ force-app/main/default/lwc/workExperienceTab/workExperienceTab.js
@@ -2,25 +2,30 @@ import { LightningElement, wire ,api } from 'lwc';
 import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 
 export default class WorkExperienceTab extends LightningElement {
-    @api recordId
-
+    @api recordId;
+    @api p;
     @wire(getRelatedListRecords,{
-        parentRecordId: '$recordId',
-        relatedListId: 'Work_Experience__r',
-        fields:['WorkExperience_c.Job_Start_Date__c',
-        'WorkExperience_c.Job_End_Date__c',
-        'WorkExperience_c.Is_Current__c',
-        'WorkExperience_c.Role__c',
-        'WorkExperience_c.CompanyName__c',
-        'WorkExperience_c.Description__c',
+        parentRecordId: 'a015g00000nwW0VAAU',
+        relatedListId: 'Work_Experiences__r',
+        fields:['Work_Experience__c.CompanyName__c',
+        'Work_Experience__c.Description__c',
+        'Work_Experience__c.Is_Current__c',
+        'Work_Experience__c.Job_End_Date__c',
+        'Work_Experience__c.Job_Start_Date__c',
+        'Work_Experience__c.Role__c'
+
        ]
     })workExperienceHandler({data,error}){
         if(data){
-            console.log(this.data)
+            console.log("WorkExperience Data ", JSON.stringify(data));
+            this.p=data;
 
         }
         if(error){
             console.log(this.error)
         }
+    }
+    datacheck(){
+        console.log(this.p);
     }
 }
