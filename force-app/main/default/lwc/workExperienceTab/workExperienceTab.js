645356dcaea25861a2d912a6f8aab3b56ad1e573
Abhishek Kumar <45291137+abhi7743@users.noreply.github.com>
2023-04-19 09:53:38 +0530

    Experience tab working

--- force-app/main/default/lwc/workExperienceTab/workExperienceTab.js
+++ force-app/main/default/lwc/workExperienceTab/workExperienceTab.js
@@ -4,6 +4,7 @@ import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 export default class WorkExperienceTab extends LightningElement {
     @api recordId;
     @api p;
+    workExperience=[]
     @wire(getRelatedListRecords,{
         parentRecordId: 'a015g00000nwW0VAAU',
         relatedListId: 'Work_Experiences__r',
@@ -18,7 +19,7 @@ export default class WorkExperienceTab extends LightningElement {
     })workExperienceHandler({data,error}){
         if(data){
             console.log("WorkExperience Data ", JSON.stringify(data));
-            this.p=data;
+            this.formatExperience(data)
 
         }
         if(error){
@@ -27,5 +28,25 @@ export default class WorkExperienceTab extends LightningElement {
     }
     datacheck(){
         console.log(this.p);
+    }
+    formatExperience(data){
+        this.workExperience=[...data.records].reverse().map(item=>{
+            let id =item.id;
+            const {Job_End_Date__c, Job_Start_Date__c, Role__c,
+                CompanyName__c, Description__c, Is_Current__c }=item.fields
+            let JobStartDate = this.getValue(Job_Start_Date__c);
+            let JobEndDate = this.getValue(Job_End_Date__c);
+            let Role = this.getValue(Role__c);
+            let CompanyName = this.getValue(CompanyName__c);
+            let Description = this.getValue(Description__c);
+            let IsCurrent= this.getValue(Is_Current__c);
+
+            return {id,JobStartDate,JobEndDate,Role,CompanyName,Description,IsCurrent}
+        })
+        
+    }
+    
+    getValue(data){
+        return data && (data.displayValue || data.value);
     }
 }
